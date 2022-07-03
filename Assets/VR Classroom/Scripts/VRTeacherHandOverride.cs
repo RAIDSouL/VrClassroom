78c47b8e5e11a41e24bf3475dd15780eeeb2f9f2
Hello <parm_51140@hotmail.com>
2022-07-03 16:44:50 +0700

    11

--- Assets/VR Classroom/Scripts/VRTeacherHandOverride.cs
+++ Assets/VR Classroom/Scripts/VRTeacherHandOverride.cs
@@ -6,22 +6,18 @@ public class VRTeacherHandOverride : MonoBehaviour
 {
    [SerializeField] SkinnedMeshRenderer leftHand, RightHand;
    [SerializeField] Material skinMat;
-    public GameObject skinRef;
+    public GameObject ObSkinRef, hidehand;
     void Start()
     {
 
         Material skinMatU = new Material(skinMat);
-     
-        skinMatU.color=new Color(1,1,1,1);
-      
+        ObSkinRef = gameObject.GetComponentInChildren<JointManager>().transform.GetChild(1).gameObject;
+        hidehand = gameObject.GetComponentInChildren<JointManager>().transform.GetChild(9).gameObject; 
+        Material[] skinRef = ObSkinRef.GetComponent<Renderer>().materials;
+        skinMatU.color = skinRef[0].color;
         leftHand.material = skinMatU;
         RightHand.material = skinMatU;
-       
+        hidehand.SetActive(false);
     }
 
-  
-    void Update()
-    {
-        
-    }
 }
